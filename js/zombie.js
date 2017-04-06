95c7e6696640dc8574d7bc7557e3410df7c48b6d
Joakim Unge <ashba@unge.se>
2017-04-06 15:57:26 +0200

    added restart game function. Wrapped game in main function.

--- js/zombie.js
+++ js/zombie.js
@@ -1,5 +1,9 @@
 /* Fak yeah zombies */
 
+zombieGame()
+
+function zombieGame() {
+
 alert("It is the zombie apocalypse. You are looting a store when suddenly a zombie bursts through the door!");
 
 var weapon = prompt("You search around frantically for a weapon. What do you choose? Bow and arrow, axe or rubber chicken?");
@@ -8,7 +12,23 @@ var randomNumber = Math.round(Math.random());
 alert("You attack the zombie with your" + " " + weapon);
 
 if (randomNumber === 0) {
-
+	alert("The zombie bites you. You lose!");
 } else if (randomNumber === 1) {
-	
+	alert("You kill the zombie with your" + " " + weapon + "!");
+}
+
+function restartGame() {
+	var response = confirm("Do you want to play again?");
+
+	if (response === true) {
+		zombieGame()
+	}
+
+	else {
+		alert("Thanks for playing!");
+	}
+}
+
+restartGame()
+
 }
