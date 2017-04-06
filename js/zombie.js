7d414ed0e80ed4f5c4124510f8466d16500b4a24
Joakim Unge <ashba@unge.se>
2017-04-06 16:11:45 +0200

    added circumstantial kill moves

--- js/zombie.js
+++ js/zombie.js
@@ -6,16 +6,8 @@ function zombieGame() {
 
 alert("It is the zombie apocalypse. You are looting a store when suddenly a zombie bursts through the door!");
 
-var weapon = prompt("You search around frantically for a weapon. What do you choose? Bow and arrow, axe or rubber chicken?");
+var weapon = prompt("You search around frantically for a weapon. What did you find?");
 var randomNumber = Math.round(Math.random());
-
-alert("You attack the zombie with your" + " " + weapon);
-
-if (randomNumber === 0) {
-	alert("The zombie bites you. You lose!");
-} else if (randomNumber === 1) {
-	alert("You kill the zombie with your" + " " + weapon + "!");
-}
 
 function restartGame() {
 	var response = confirm("Do you want to play again?");
@@ -25,9 +17,24 @@ function restartGame() {
 	}
 
 	else {
-		alert("Thanks for playing!");
+		alert("Thanks for playing this short ass game!");
 	}
 }
+
+alert("You attack the zombie with your" + " " + weapon);
+
+if (randomNumber === 0) {
+	alert("The zombie bites you. You lose!");
+} else if (randomNumber === 1 && weapon === "hammer") {
+	alert("Zombie is smashed into a puddle of ass.");
+} else if (randomNumber === 1 && weapon === "dick") {
+	alert("You rape zombie");
+}else if (randomNumber === 1) {
+	alert("You kill the zombie with your" + " " + weapon + "!");
+}
+
+
+
 
 restartGame()
 
