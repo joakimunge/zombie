651ab8c789d5895f4feee6ae8205e0f29442441e
Joakim Unge <ashba@unge.se>
2017-04-06 15:33:40 +0200

    cont.

---
+++ js/zombie.js
@@ -0,0 +1,14 @@
+/* Fak yeah zombies */
+
+alert("It is the zombie apocalypse. You are looting a store when suddenly a zombie bursts through the door!");
+
+var weapon = prompt("You search around frantically for a weapon. What do you choose? Bow and arrow, axe or rubber chicken?");
+var randomNumber = Math.round(Math.random());
+
+alert("You attack the zombie with your" + " " + weapon);
+
+if (randomNumber === 0) {
+
+} else if (randomNumber === 1) {
+	
+}
